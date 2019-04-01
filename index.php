bd92fcd19c7ae19ba4c33e2b488c31873af3fccb
janvb2001 <49169548+janvb2001@users.noreply.github.com>
2019-04-01 22:08:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST"){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
+    
+    $eigenschappen = $json->result->parameters->Eigenschappen;
+
+    switch ($eigenschappen) {
+        case "Opslagruimte":
+            $speech = "Je zoekt naar de opslagruimte";
+            break;
+        case "Geheugen":
+            $speech = "Je zoekt naar het geheugen";
+            break;
+        case "Scherm"
+            $speech = "Je zoekt naar het formaat van het scherm";
+            break;
+        default:
+            $speech = "Ik kan niet vinden wat je zoekt.";
+            break;
+    }
+
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
+
+}
+else
+{
+    echo "Method not allowed";
+}
+
+
+
+?>
