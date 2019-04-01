d49c60441f308e167448105488048fd333f6508f
janvb2001 <49169548+janvb2001@users.noreply.github.com>
2019-04-01 23:21:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,11 +26,7 @@ if($method == "POST"){
     
 
     $response = new \stdClass();
-    $response->responseId = $responseId;
     $response->fulfillmentText = $speech;
-    $response->queryResult = $speech;
-    $response->speech = $speech;
-    $response->source = "webhook";
     echo json_encode($response);
 
 }
