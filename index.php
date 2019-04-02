14a1dd47203174a4c5dee4a00d3c654457f9b345
janvb2001 <49169548+janvb2001@users.noreply.github.com>
2019-04-02 16:25:03 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1><?php echo $_SESSION["eigenschap"]; ?></h1>
+    </body>
+</html>
